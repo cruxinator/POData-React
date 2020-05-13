3356f787c0eb419618b3f2735d86d01a4ca68de0
c-harris <c.harris@communicationinnovations.com.au>
2020-05-13 22:12:22 +1000

    automated code clean

--- src/OData.js
+++ src/OData.js
@@ -1,53 +1,53 @@
-import React, { useState } from 'react';
-import Fetch, { useFetch, FetchContext } from 'react-fetch-component';
+import React, {useState} from 'react';
+import {FetchContext, useFetch} from 'react-fetch-component';
 import {QueryBuilder} from 'odata-query-builder';
 
-import { isFunction, isEmpty } from './utils';
+import {isEmpty, isFunction} from './utils';
 
 function buildUrl(baseUrl, query) {
-  return query !== false && baseUrl + (isEmpty(query) ? '' : query.toQuery());
+    return query !== false && baseUrl + (isEmpty(query) ? '' : query.toQuery());
 }
 
-function useOData({ baseUrl, defaultQuery, query, ...props }) {
-  const [state, setState] = useState({
-    query: defaultQuery
-  });
-  state.setQuery = (updater, cb) =>
-    setState(
-      prevState => ({
-        query: {
-          ...prevState.query,
-          ...(isFunction(updater) ? updater(prevState.query) : updater)
-        }
-      }),
-      cb
+function useOData({baseUrl, defaultQuery, query, ...props}) {
+    const [state, setState] = useState({
+        query: defaultQuery
+    });
+    state.setQuery = (updater, cb) =>
+        setState(
+            prevState => ({
+                query: {
+                    ...prevState.query,
+                    ...(isFunction(updater) ? updater(prevState.query) : updater)
+                }
+            }),
+            cb
+        );
+
+    const fetchState = useFetch({
+        url: query !== false && buildUrl(baseUrl, {...query, ...state.query}),
+        fetchFunction: (url, options, updateOptions) => {
+            url = typeof url === 'string' ? url : buildUrl(baseUrl, url);
+            return fetch(url, options, updateOptions);
+        },
+        ...props
+    });
+
+    return {...fetchState, ...state};
+}
+
+const POData = ({children, ...props}) => {
+    const state = useOData(props);
+    return (
+        <FetchContext.Provider value={state}>
+            {isFunction(children) ? (
+                <FetchContext.Consumer>{children}</FetchContext.Consumer>
+            ) : (
+                children
+            )}
+        </FetchContext.Provider>
     );
-
-  const fetchState = useFetch({
-    url: query !== false && buildUrl(baseUrl, { ...query, ...state.query }),
-    fetchFunction: (url, options, updateOptions) => {
-      url = typeof url === 'string' ? url : buildUrl(baseUrl, url);
-      return fetch(url, options, updateOptions);
-    },
-    ...props
-  });
-
-  return { ...fetchState, ...state };
-}
-
-const POData = ({ children, ...props }) => {
-  const state = useOData(props);
-  return (
-    <FetchContext.Provider value={state}>
-      {isFunction(children) ? (
-        <FetchContext.Consumer>{children}</FetchContext.Consumer>
-      ) : (
-        children
-      )}
-    </FetchContext.Provider>
-  );
 };
 POData.Consumer = FetchContext.Consumer;
 
-export { useOData, QueryBuilder };
+export {useOData, QueryBuilder};
 export default POData;
--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 export const isFunction = value => typeof value === 'function';
 export const isEmpty = obj => {
-    for(let key in obj) {
-        if(obj.hasOwnProperty(key)) {
+    for (let key in obj) {
+        if (obj.hasOwnProperty(key)) {
             return false;
         }
     }
